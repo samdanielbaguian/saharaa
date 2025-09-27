a54edcc451410e5505ed78dbf1974e01ee1c9f9c
samdanielbaguian <baguiandaniel01@gmail.com>
2025-09-27 16:20:22 -0400

    Add files via upload

---
+++ src/utils/generateOtp.ts
@@ -0,0 +1,7 @@
+export const generateOtp = (length: number = 6): string => {
+  let otp = '';
+  for (let i = 0; i < length; i++) {
+    otp += Math.floor(Math.random() * 10).toString();
+  }
+  return otp;
+};

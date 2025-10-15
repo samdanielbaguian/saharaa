126f511b622742f54919f959854af85a038d9716
samdanielbaguian <baguiandaniel01@gmail.com>
2025-10-14 20:24:24 -0400

    Set up Sequelize configuration for PostgreSQL

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,1 +1,11 @@
+import { Sequelize } from 'sequelize';
+import { env } from './env';
 
+const sequelize = new Sequelize(env.dbName, env.dbUser, env.dbPass, {
+  host: env.dbHost,
+  port: Number(env.dbPort),
+  dialect: 'postgres', // Mets ton SGBD ici si besoin
+  logging: false,      // Mets true pour voir les requêtes SQL
+});
+
+export default sequelize;

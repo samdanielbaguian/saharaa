465048fb0a74a26170371de23837710fbed1899a
samdanielbaguian <baguiandaniel01@gmail.com>
2025-10-14 20:26:27 -0400

    Delete src directory

--- src/utils/validate.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Vérifie si une adresse email est valide.
- */
-export const validateEmail = (email: string): boolean => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-};
